e9a23cd06e88ab371934eb6afa5d83db28cbf74f
Toshifumi Kurosawa <kuromailserver@gmail.com>
2022-10-21 22:26:25 +0900

    s

--- app/routes/sampleRoutes.js
+++ app/routes/sampleRoutes.js
@@ -3,14 +3,25 @@ const router = express.Router()
 
 const sampleController = require('../controllers/sampleController.js')
 
+// C
 router.get('/new', sampleController.newSample)
-router.delete('/:id', sampleController.deleteSample)
 router.post('/', sampleController.createSample)
+
+// R
 router.get('/', sampleController.indexSamples)
 router.get('/:id', sampleController.showSample)
+// U
 router.get('/edit/:id', sampleController.editSample)
 router.put('/:id', sampleController.updateSample)
 
-router.get('/search/:keyword', sampleController.searchSamples)
+// D
+router.delete('/:id', sampleController.deleteSample)
+
+
+
+
+
+
+// router.get('/search/:keyword', sampleController.search)
 
 module.exports = router
