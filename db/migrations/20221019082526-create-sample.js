f129fb72cc70ca05ef8c5c0ae7b7a84c3c56095a
Toshifumi Kurosawa <kuromailserver@gmail.com>
2022-10-22 12:13:48 +0900

    s

--- db/migrations/20221019082526-create-sample.js
+++ db/migrations/20221019082526-create-sample.js
@@ -12,6 +12,9 @@ module.exports = {
       name: {
         type: Sequelize.STRING
       },
+      image: {
+        type: Sequelize.STRING
+      },
       created_at: {
         type: Sequelize.DATE
       },
--- db/seeders/20221019102526-fake_sample.js
+++ db/seeders/20221019102526-fake_sample.js
@@ -10,14 +10,17 @@ module.exports = {
       [
         {
           name: "sample 1",
+          image: faker.image.image(),
           created_at: faker.date.between('2020-01-01T00:00:00.000Z', '2022-01-01T00:00:00.000Z'),
           updated_at: faker.date.between('2020-01-01T00:00:00.000Z', '2022-01-01T00:00:00.000Z'),
         }, {
           name: "sample 2",
+          image: faker.image.image(),
           created_at: faker.date.between('2020-01-01T00:00:00.000Z', '2022-01-01T00:00:00.000Z'),
           updated_at: faker.date.between('2020-01-01T00:00:00.000Z', '2022-01-01T00:00:00.000Z'),
         }, {
           name: "sample 3",
+          image: faker.image.image(),
           created_at: faker.date.between('2020-01-01T00:00:00.000Z', '2022-01-01T00:00:00.000Z'),
           updated_at: faker.date.between('2020-01-01T00:00:00.000Z', '2022-01-01T00:00:00.000Z'),
         }
